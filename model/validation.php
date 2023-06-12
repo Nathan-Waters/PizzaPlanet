f95f1bcfb4741a103ad304bd831df713343476a7
grigorenko.sam@student.greenriver.edu <samgree1@srv604.webhostingforstudents.com>
2023-06-12 01:34:53 -0400

    finished up the validation functions

--- model/validation.php
+++ model/validation.php
@@ -1,8 +1,30 @@
 <?php
 
 //validate crust
+function validSelectedCrust($selectedCrust){
+    $validCrust = getCrust();
+
+    //Check each user toppings against array of valid toppings
+    foreach ($selectedCrust as $selectedCrust){
+        if (!in_array($selectedCrust, $validCrust)){
+            return false;
+        }
+    }
+    return true;
+}
 
 //validate sauce
+function validSelectedSauce($selectedSauce){
+    $validSauce = getSauce();
+
+    //Check each user toppings against array of valid toppings
+    foreach ($selectedSauce as $selectedSauce){
+        if (!in_array($selectedSauce, $validSauce)){
+            return false;
+        }
+    }
+    return true;
+}
 
 //validate toppings
 function validSelectedToppings($selectedToppings){
@@ -17,5 +39,15 @@ function validSelectedToppings($selectedToppings){
     return true;
 }
 
-//validate size
+//validate Size
+function validSelectedSize($selectedSize){
+    $validSize = getSize();
 
+    //Check each user toppings against array of valid toppings
+    foreach ($selectedSize as $selectedSize){
+        if (!in_array($selectedSize, $validSize)){
+            return false;
+        }
+    }
+    return true;
+}
